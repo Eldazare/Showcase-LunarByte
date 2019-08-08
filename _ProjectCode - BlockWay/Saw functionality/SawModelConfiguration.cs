954b2e129ba87cdf7a48e1aedfaa4dadedd48294
Sakari Helokunnas <sakari.ryuzaki@gmail.com>
2019-08-08 12:00:05 +0300

    Update SawModelConfiguration.cs

--- _ProjectCode - BlockWay/Saw functionality/SawModelConfiguration.cs
+++ _ProjectCode - BlockWay/Saw functionality/SawModelConfiguration.cs
@@ -6,25 +6,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = nameof(SawModelConfiguration), menuName = "Saw/Configurations/" + nameof(SawModelConfiguration))]
 public class SawModelConfiguration : ModelConfiguration<ISawModel, SawModel, ISawSettings, SaveModel>
 {
-	protected override void Configure(SawModel model, ISawSettings settings, SaveModel saveModel)
+    protected override void Configure(SawModel model, ISawSettings settings, SaveModel saveModel)
+    {
+	Debug.Log($"Configure {nameof(SawModelConfiguration)}");
+
+	if (saveModel.SaveObjectOld.CurrentSawSpeed != 0)
 	{
-		Debug.Log($"Configure {nameof(SawModelConfiguration)}");
-
-		if (saveModel.SaveObjectOld.CurrentSawSpeed != 0)
-		{
-			model.SawAutoSpeed = saveModel.SaveObjectOld.CurrentSawSpeed;
-		}
-		else
-		{
-			model.SawAutoSpeed = settings.SawAutoSpeed;
-
-        }
-        model.SawCurve = settings.SawCurve;
-		model.SawPos = settings.SawPos;
-		model.SawRotationSpeed = settings.SawRotationSpeed;
-		model.SuperHayContainerMax = settings.SuperHayContainerMax;
-		model.SuperSawDuration = settings.SuperSawDuration;
-		model.SuperSawScaleMultiplier = settings.SuperSawScaleMultiplier;
-		model.SawScaleBase = settings.SawScaleBase;
+	    model.SawAutoSpeed = saveModel.SaveObjectOld.CurrentSawSpeed;
 	}
+	else
+	{
+	    model.SawAutoSpeed = settings.SawAutoSpeed;
+
+	}
+        model.SawCurve = settings.SawCurve;
+	model.SawPos = settings.SawPos;
+	model.SawRotationSpeed = settings.SawRotationSpeed;
+	model.SuperHayContainerMax = settings.SuperHayContainerMax;
+	model.SuperSawDuration = settings.SuperSawDuration;
+	model.SuperSawScaleMultiplier = settings.SuperSawScaleMultiplier;
+	model.SawScaleBase = settings.SawScaleBase;
+    }
 }
