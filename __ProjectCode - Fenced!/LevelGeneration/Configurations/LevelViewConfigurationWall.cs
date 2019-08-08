0bc3298487dd23930841b1450fbe91b6f5eb5727
Sakari Helokunnas <sakari.ryuzaki@gmail.com>
2019-08-08 11:25:59 +0300

    Update LevelViewConfigurationWall.cs

--- __ProjectCode - Fenced!/LevelGeneration/Configurations/LevelViewConfigurationWall.cs
+++ __ProjectCode - Fenced!/LevelGeneration/Configurations/LevelViewConfigurationWall.cs
@@ -18,26 +18,30 @@ public class LevelViewConfigurationWall : ViewConfiguration<ILevelViewModelWall,
     {
         Debug.Log($"Configuring {nameof(LevelViewConfigurationWall)}");
 
-        viewModel.Bind<LevelViewModelWall, ILevelDataWall>(levelData => viewModel.LevelData = levelData).ToProperty(levelModel, m => m.LevelData, nameof(LevelModelWall.LevelData));
-        view.Bind<LevelViewWall, ILevelDataWall>(view.LevelDataChanged).ToProperty(viewModel, vm => vm.LevelData, nameof(LevelViewModelWall.LevelData));
+	//Property binds
+        viewModel.Bind<LevelViewModelWall, ILevelDataWall>(levelData => viewModel.LevelData = levelData)
+		.ToProperty(levelModel, m => m.LevelData, nameof(LevelModelWall.LevelData));
+		
+        view.Bind<LevelViewWall, ILevelDataWall>(view.LevelDataChanged)
+		.ToProperty(viewModel, vm => vm.LevelData, nameof(LevelViewModelWall.LevelData));
 
-        viewModel.Bind<LevelViewModelWall, LevelCommand>(command => viewModel.Command = command).ToProperty(levelModel, m => m.Command, nameof(LevelModelWall.Command));
-        view.Bind<LevelViewWall, LevelCommand>(view.ReceiveLevelCommand).ToProperty(viewModel, vm => vm.Command, nameof(LevelViewModelWall.Command));
+        viewModel.Bind<LevelViewModelWall, LevelCommand>(command => viewModel.Command = command)
+		.ToProperty(levelModel, m => m.Command, nameof(LevelModelWall.Command));
+		
+        view.Bind<LevelViewWall, LevelCommand>(view.ReceiveLevelCommand)
+		.ToProperty(viewModel, vm => vm.Command, nameof(LevelViewModelWall.Command));
 
-
-        viewModel
-	        .Bind<LevelViewModelWall, int>(endLevelKeys => viewModel.EndLevelKeys = endLevelKeys)
+        viewModel.Bind<LevelViewModelWall, int>(endLevelKeys => viewModel.EndLevelKeys = endLevelKeys)
 	        .ToProperty(playerModel, m => m.KeyRing[0], nameof(PlayerModel.KeyRing));
 
         view.Bind<LevelViewWall, int>((int i) => view.AttemptRemoveEndDoorKeys(0))
             .ToProperty(viewModel, vm => vm.EndLevelKeys, nameof(LevelViewModelWall.EndLevelKeys));
 
-
+	//Event binds
         viewModel.ResetPlayerModelEvent.AddListener(() => playerModel.ResetPlayer());
         viewModel.ResetInputEvent.AddListener(() => inputModel.ResetInput());
         viewModel.ChooseNextLevelEvent.AddListener(() => levelModel.SetNextLevel(levelDataContainer));  
-		viewModel.AttemptRemoveAllKeysEvent.AddListener((int ind, Action successAction) => playerModel.AttemptRemoveMaxKeys(ind, successAction, levelModel.KeysMax[ind]));
-
+	viewModel.AttemptRemoveAllKeysEvent.AddListener((int ind, Action successAction) => playerModel.AttemptRemoveMaxKeys(ind, successAction, levelModel.KeysMax[ind]));
         viewModel.UpdateMaxTileObjectsEvent.AddListener((int[] keys, int collectibles) => levelModel.UpdateMaxTileObjects(keys, collectibles));
     }
 }
