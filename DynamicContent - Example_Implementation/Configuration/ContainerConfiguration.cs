42fb90b5e4f8d3f671f3bd65c93fca23ab9c2c24
Sakari Helokunnas <sakari.ryuzaki@gmail.com>
2019-08-09 10:44:37 +0300

    Update ContainerConfiguration.cs

--- DynamicContent - Example_Implementation/Configuration/ContainerConfiguration.cs
+++ DynamicContent - Example_Implementation/Configuration/ContainerConfiguration.cs
@@ -9,59 +9,45 @@ using Zenject;
 [CreateAssetMenu(fileName = nameof(ContainerConfiguration), menuName = ("Configurations/"+nameof(ContainerConfiguration)))]
 public class ContainerConfiguration : ScriptableObjectInstaller
 {
-	[SerializeField] private PrefabSettings prefabSettings;
+    [SerializeField] private PrefabSettings prefabSettings;
+
+    public override void InstallBindings()
+    {
+	Debug.Log("Configure " + nameof(ContainerConfiguration));
+	DynamicModelContainer<TestModel> modelContainer = new DynamicModelContainer<TestModel>();
+	modelContainer.Add(new TestModel(){Value = 0});
+	modelContainer.Add(new TestModel() { Value = 1 });
 
 
-	public override void InstallBindings()
-	{
-		Debug.Log("Configure " + nameof(ContainerConfiguration));
-		DynamicModelContainer<TestModel> modelContainer = new DynamicModelContainer<TestModel>();
-		modelContainer.Add(new TestModel(){Value = 0});
-		modelContainer.Add(new TestModel() { Value = 1 });
-
-
-        Container.Bind<PrefabSettings>().FromInstance(prefabSettings);
-		Container.Bind<DynamicModelContainer<TestModel>>().FromInstance(modelContainer).AsSingle();
-
-
-        Container.BindPoolContainer<TestView, ITestViewModel, TestViewFactory>(prefabSettings.PrefabList);
-
-		
-	}
+	Container.Bind<PrefabSettings>().FromInstance(prefabSettings);
+	Container.Bind<DynamicModelContainer<TestModel>>().FromInstance(modelContainer).AsSingle();
+	Container.BindPoolContainer<TestView, ITestViewModel, TestViewFactory>(prefabSettings.PrefabList);	
+    }
 }
 
-public class ViewPoolFactory<T, TViewModel> : PlaceholderFactory<int, TViewModel, T>
-{
-
-}
+public class ViewPoolFactory<T, TViewModel> : PlaceholderFactory<int, TViewModel, T> { }
 
 public class TestViewFactory : ViewPoolFactory<TestView, ITestViewModel> { }
 
 
 public static class ContainerExtensionVerTest
 {
-	public static void BindPoolContainer<TView, TViewModel, ViewPoolFactory>(this DiContainer container, GameObject[] prefabList)
- where TView: DynamicView<TViewModel>
-    where ViewPoolFactory : ViewPoolFactory<TView,TViewModel>
-	{
-
-		ViewPoolContainer<TView, TViewModel> viewPoolContainer = new ViewPoolContainer<TView, TViewModel>(prefabList, container);
+    public static void BindPoolContainer<TView, TViewModel, ViewPoolFactory>(this DiContainer container, GameObject[] prefabList)
+ 	    where TView: DynamicView<TViewModel>
+	    where ViewPoolFactory : ViewPoolFactory<TView,TViewModel>
+    {
+	ViewPoolContainer<TView, TViewModel> viewPoolContainer = new ViewPoolContainer<TView, TViewModel>(prefabList, container);
 
         container.BindFactory<int, TViewModel, TView, ViewPoolFactory>()
-		         .FromMethod((cont, index, viewModel) =>
-			                     viewPoolContainer.SpawnFromPool(index, viewModel));
-
+		 .FromMethod((cont, index, viewModel) => viewPoolContainer.SpawnFromPool(index, viewModel));
     }
 
-	public static void BindPoolContainer<T, TViewModel>(this DiContainer container, GameObject[] prefabList)
-		where T : DynamicView<TViewModel>
-	{
+    public static void BindPoolContainer<T, TViewModel>(this DiContainer container, GameObject[] prefabList)
+	    where T : DynamicView<TViewModel>
+    {
+	ViewPoolContainer<T, TViewModel> viewPoolContainer = new ViewPoolContainer<T, TViewModel>(prefabList, container);
 
-		ViewPoolContainer<T, TViewModel> viewPoolContainer = new ViewPoolContainer<T, TViewModel>(prefabList, container);
-
-		container.BindFactory<int, TViewModel, T, ViewPoolFactory<T, TViewModel>>()
-		         .FromMethod((cont, index, viewModel) =>
-			                     viewPoolContainer.SpawnFromPool(index, viewModel));
-
-	}
+	container.BindFactory<int, TViewModel, T, ViewPoolFactory<T, TViewModel>>()
+		 .FromMethod((cont, index, viewModel) => viewPoolContainer.SpawnFromPool(index, viewModel));
+    }
 }
