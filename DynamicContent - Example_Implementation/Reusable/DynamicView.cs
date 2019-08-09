a00b3f217e5be826c5a764b234ba68df80084780
Sakari Helokunnas <sakari.ryuzaki@gmail.com>
2019-08-09 10:45:58 +0300

    Update DynamicView.cs

--- DynamicContent - Example_Implementation/Reusable/DynamicView.cs
+++ DynamicContent - Example_Implementation/Reusable/DynamicView.cs
@@ -7,28 +7,28 @@ using Zenject;
 
 public class DynamicView<TViewModel> : View, IPoolable<TViewModel, IMemoryPool>, IDisposable
 {
-	protected TViewModel  ViewModelField;
-	private   IMemoryPool pool;
+    protected TViewModel  ViewModelField;
+    private   IMemoryPool pool;
 
-	public TViewModel ViewModel
-	{
-		get { return ViewModelField; }
-	}
+    public TViewModel ViewModel
+    {
+	get { return ViewModelField; }
+    }
+    
+    public void OnSpawned(TViewModel viewModel, IMemoryPool pool1)
+    {
+	ViewModelField = viewModel;
+	transform.position = Vector3.zero;
+	pool = pool1;
+    }
 
-	public void OnSpawned(TViewModel viewModel, IMemoryPool pool1)
-	{
-		ViewModelField = viewModel;
-		transform.position = Vector3.zero;
-		pool = pool1;
-	}
+    public void OnDespawned()
+    {
+	pool = null;
+    }
 
-	public void OnDespawned()
-	{
-		pool = null;
-	}
-
-	public void Dispose()
-	{
-		pool.Despawn(this);
-	}
+    public void Dispose()
+    {
+	pool.Despawn(this);
+    }
 }
