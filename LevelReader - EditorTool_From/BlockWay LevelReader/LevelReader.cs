497df1f532e8365624d64419ac27679d7e9d858f
Sakari Helokunnas <sakari.ryuzaki@gmail.com>
2019-08-09 10:37:18 +0300

    Update LevelReader.cs

--- LevelReader - EditorTool_From/BlockWay LevelReader/LevelReader.cs
+++ LevelReader - EditorTool_From/BlockWay LevelReader/LevelReader.cs
@@ -7,17 +7,17 @@ using UnityEngine;
 
 public class LevelReader : EditorWindow
 {
-	//Single fields
+    //Single fields
     public TextAsset CsvFile;
     public string ScriptableObjectName;
     public bool CreateNewScriptableObject;
 
-	//Multi Fields
+    //Multi Fields
     public  TextAsset[] CsvFiles;
     public  string      ScriptableObjectBaseName;
     public  int         ScriptableObjectNameFirstNumber;
 
-	//Fields
+    //Fields
     public static string ParentFolderPath;
     private LevelData LevelData;
     private PrefabSettings prefabSettings;
@@ -28,7 +28,7 @@ public class LevelReader : EditorWindow
     [MenuItem("Assets/Create/LevelConversion/LevelReaderSingle")]
     protected static void InitSingle()
     {
-	    UseSingle = true;
+	UseSingle = true;
         Rect posAndSize = new Rect(0, 0, 600, 130);
         EditorWindow window = GetWindowWithRect(typeof(LevelReader), posAndSize);
         window.titleContent.text = "LevelConverterCSVtoScriptable";
@@ -39,89 +39,91 @@ public class LevelReader : EditorWindow
     [MenuItem("Assets/Create/LevelConversion/LevelReaderMulti")]
     protected static void InitMulti()
     {
-	    UseSingle = false;
-	    Rect posAndSize = new Rect(0, 0, 600, 130);
-	    EditorWindow window = GetWindowWithRect(typeof(LevelReader), posAndSize);
-	    window.titleContent.text = "LevelConverterCSVtoScriptableMulti";
-	    ParentFolderPath = AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID());
-	    window.Show();
+	UseSingle = false;
+	Rect posAndSize = new Rect(0, 0, 600, 130);
+	EditorWindow window = GetWindowWithRect(typeof(LevelReader), posAndSize);
+	window.titleContent.text = "LevelConverterCSVtoScriptableMulti";
+	ParentFolderPath = AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID());
+	window.Show();
     }
 
     void OnGUI()
     {
-	    EditorWindow window = this;
-	    prefabSettings = (PrefabSettings)EditorGUILayout.ObjectField(
-		    "Prefab settings (char data):", prefabSettings, typeof(PrefabSettings), false);
+	EditorWindow window = this;
+	prefabSettings = (PrefabSettings)EditorGUILayout.ObjectField
+	(
+		    "Prefab settings (char data):", 
+		    prefabSettings, 
+		    typeof(PrefabSettings), 
+		    false
+	);
 
 
         if (UseSingle)
-	    {
+	{
 
-            GUILayout.Label("Convert CSV level files to Target type");
+	    GUILayout.Label("Convert CSV level files to Target type");
 
-		    CsvFile = (TextAsset) EditorGUILayout.ObjectField(
+	    CsvFile = (TextAsset) EditorGUILayout.ObjectField(
 			    "SourceCSV: ", CsvFile, typeof(TextAsset), false);
 
-		    LevelData = (LevelData) EditorGUILayout.ObjectField(
+	    LevelData = (LevelData) EditorGUILayout.ObjectField(
 			    "Existing Target:", LevelData, typeof(LevelData), false);
 
-		    CreateNewScriptableObject =
-			    EditorGUILayout.Toggle("CreateNewTarget:", CreateNewScriptableObject);
-		    ScriptableObjectName = EditorGUILayout.TextField("NewSOName", ScriptableObjectName);
+	    CreateNewScriptableObject = EditorGUILayout.Toggle("CreateNewTarget:", CreateNewScriptableObject);
+	    ScriptableObjectName = EditorGUILayout.TextField("NewSOName", ScriptableObjectName);
 
-		    if (GUILayout.Button("CONVERT"))
-		    {
-			    LevelDataContainerTemplate<int> container = ParseCsv(CsvFile);
+	    if (GUILayout.Button("CONVERT"))
+	    {
+		LevelDataContainerTemplate<int> container = ParseCsv(CsvFile);
 
-			    if (CreateNewScriptableObject)
-			    {
-				    LevelData = (LevelData)ScriptableObject.CreateInstance(typeof(LevelData));
+		if (CreateNewScriptableObject)
+		{
+		    LevelData = (LevelData)ScriptableObject.CreateInstance(typeof(LevelData));
 
-				    AssetDatabase.CreateAsset(LevelData,
-				                              $"{ParentFolderPath}/{ScriptableObjectName}.asset");
-			    }
+		    AssetDatabase.CreateAsset(LevelData, $"{ParentFolderPath}/{ScriptableObjectName}.asset");
+		}
 
-			    LevelData.LevelObjects = container.ObjectList.ToArray();
+		LevelData.LevelObjects = container.ObjectList.ToArray();
                 EditorUtility.SetDirty(LevelData);
 
-			    this.Close();
-		    }
+		this.Close();
 	    }
-	    else
+	}
+	else
+	{
+	    GUILayout.Label("Convert list of CSV files to Target type ");
+	    ScriptableObjectBaseName =
+	    EditorGUILayout.TextField("TargetBaseName", ScriptableObjectBaseName);
+
+	    ScriptableObjectNameFirstNumber =
+	    EditorGUILayout.IntField("StartNumberingFrom...", ScriptableObjectNameFirstNumber);
+
+	    ScriptableObject target = this;
+	    SerializedObject so = new SerializedObject(target);
+	    SerializedProperty CsvFilesProperty = so.FindProperty("CsvFiles");
+	    EditorGUILayout.PropertyField(CsvFilesProperty, true);
+	    so.ApplyModifiedProperties();
+
+
+	    if (GUILayout.Button("CONVERT"))
 	    {
-		    GUILayout.Label("Convert list of CSV files to Target type ");
-		    ScriptableObjectBaseName =
-			    EditorGUILayout.TextField("TargetBaseName", ScriptableObjectBaseName);
+		int i = 0;
+		foreach (var file in CsvFiles)
+		{
+		    LevelDataContainerTemplate<int> container = ParseCsv(file);
 
-		    ScriptableObjectNameFirstNumber =
-			    EditorGUILayout.IntField("StartNumberingFrom...", ScriptableObjectNameFirstNumber);
+		    LevelData = (LevelData)ScriptableObject.CreateInstance(typeof(LevelData));
 
-		    ScriptableObject target = this;
-		    SerializedObject so = new SerializedObject(target);
-		    SerializedProperty CsvFilesProperty = so.FindProperty("CsvFiles");
-		    EditorGUILayout.PropertyField(CsvFilesProperty, true);
-		    so.ApplyModifiedProperties();
+		    AssetDatabase.CreateAsset(LevelData,$"{ParentFolderPath}/{ScriptableObjectBaseName}{ScriptableObjectNameFirstNumber + i}.asset");
 
+		    LevelData.LevelObjects = container.ObjectList.ToArray();
+		    EditorUtility.SetDirty(LevelData);
+		    i++;
+		}
 
-		    if (GUILayout.Button("CONVERT"))
-		    {
-			    int i = 0;
-			    foreach (var file in CsvFiles)
-			    {
-				    LevelDataContainerTemplate<int> container = ParseCsv(file);
-
-				    LevelData = (LevelData)ScriptableObject.CreateInstance(typeof(LevelData));
-
-				    AssetDatabase.CreateAsset(LevelData,
-				                              $"{ParentFolderPath}/{ScriptableObjectBaseName}{ScriptableObjectNameFirstNumber + i}.asset");
-
-				    LevelData.LevelObjects = container.ObjectList.ToArray();
-				    EditorUtility.SetDirty(LevelData);
-				    i++;
-			    }
-
-			    this.Close();
-		    }
+		this.Close();
+	    }
         }
     }
 
@@ -132,9 +134,9 @@ public class LevelReader : EditorWindow
         cont.Data1 = int.Parse(lines[0].Split(';')[1]);
         cont.Data2 = double.Parse(lines[1].Split(';')[1].Replace('.', ','));
 
-		char[] chars = prefabSettings.LevelReaderCharList;
+	char[] chars = prefabSettings.LevelReaderCharList;
 
-		StringBuilder singleLineBuilder = new StringBuilder();
+	StringBuilder singleLineBuilder = new StringBuilder();
         for (int i = lines.Length - 1; i > 1; i--)
         {
             singleLineBuilder.Append(ReplaceRulesLine(lines[i]));
@@ -143,13 +145,12 @@ public class LevelReader : EditorWindow
 
         foreach (char s in singleLineBuilder.ToString())
         {
-
-	        for (int i = 0; chars.Length>i;i++)
-	        {
-		        if (chars[i] == s)
-		        {
-					cont.ObjectList.Add(i);
-		        }
+	    for (int i = 0; chars.Length>i;i++)
+	    {
+		if (chars[i] == s)
+		{
+		    cont.ObjectList.Add(i);
+		}
             }
         }
 
